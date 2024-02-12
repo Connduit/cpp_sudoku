3da9ab1017356a0a798163e2391afb5f103f7725
Connor Finch <49081590+Connduit@users.noreply.github.com>
2024-02-12 17:44:29 -0500

    Create board.h

---
+++ board.h
@@ -0,0 +1,20 @@
+#include "tile.h"
+#include <vector>
+#include <set>
+
+#define SIZE 9
+
+class Board {
+public:
+  Board();
+  Board(std::vector<std::vector<int>> puzzle);
+  std::vector<std::vector<Tile>> board;
+  std::vector<Tile> getRowNeighbors(Tile tile);
+  std::vector<Tile> getColNeighbors(Tile tile);
+  std::vector<Tile> getGroupNeighbors(Tile tile);
+  std::set<int> getNeighbors(Tile tile);
+  // std::set<int> lastCandidate(Tile tile);
+  void printBoard();
+  void solve();
+  bool solved();
+}
